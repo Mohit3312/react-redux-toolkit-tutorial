042887f164404bc2ea18a224233c5d5b90ab41c3
Mohit Kumar <mohit3312@gmail.com>
2025-11-16 00:47:00 +0530

    Remove Data from Redux Store | Remove Item & Clear Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import Header from "./Header";
 import "./App.css";
 import Product from "./Product";
+import { useDispatch } from "react-redux";
+import { clearAllItems } from "./redux/slice";
 
 function App() {
+  const dispatch = useDispatch();
   return (
     <>
       <Header />
       <h1>React Redux Toolkit Tutorial</h1>
+      <button
+        className="clear-cart-btn"
+        onClick={() => dispatch(clearAllItems())}
+      >
+        Clear Cart
+      </button>
       <Product />
     </>
   );
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { addItem } from "./redux/slice";
+import { addItem, removeItem } from "./redux/slice";
 
 const Product = () => {
   const dispatch = useDispatch();
@@ -19,14 +19,38 @@ const Product = () => {
         >
           Add to Cart
         </button>
+        <button
+          className="remove-btn"
+          onClick={() => {
+            dispatch(removeItem(1));
+            console.log("Clicked");
+          }}
+        >
+          Remove from Cart
+        </button>
       </div>
 
       <div className="product-card">
         <img src="https://via.placeholder.com/250" alt="Product" />
         <h2>Wireless Mouse</h2>
         <p className="price">₹599</p>
-        <button className="add-btn" onClick="addToCart()">
+        <button
+          className="add-btn"
+          onClick={() => {
+            dispatch(addItem(1));
+            console.log("Clicked");
+          }}
+        >
           Add to Cart
+        </button>
+        <button
+          className="remove-btn"
+          onClick={() => {
+            dispatch(removeItem(1));
+            console.log("Clicked");
+          }}
+        >
+          Remove from Cart
         </button>
       </div>
 
@@ -34,8 +58,23 @@ const Product = () => {
         <img src="https://via.placeholder.com/250" alt="Product" />
         <h2>Smartwatch</h2>
         <p className="price">₹2,499</p>
-        <button className="add-btn" onClick="addToCart()">
+        <button
+          className="add-btn"
+          onClick={() => {
+            dispatch(addItem(1));
+            console.log("Clicked");
+          }}
+        >
           Add to Cart
+        </button>
+        <button
+          className="remove-btn"
+          onClick={() => {
+            dispatch(removeItem(1));
+            console.log("Clicked");
+          }}
+        >
+          Remove from Cart
         </button>
       </div>
     </section>
